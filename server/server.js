63f2edef99313b7d1d4fa46fa5a4cf0501e39bc3
shihadcredot <shihad@credot.dev>
2024-03-01 17:14:07 +0530

    fix-connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 
 import express from "express"
-import React from "react"
+
 // import MyComponent from "../src/MyComponent" 
+import cors from "cors"
+import bodyParser from "body-parser"
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
-app.get("/", (req, res) => {
-    
-  //render MyComponent here
-  res.send();
+app.post("/login", (req, res) => {
+try {
+  console.log(req.body,);
+    const { name } = req.body;
+    if(name){
+ 
+      res.json({ name  });
+    }
+    return res.json({ mesage:"please enter a name" });
+} catch (error) {
+  console.log(error);
+}
+
 })
 
 app.listen(3000, () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
+import MyComponent from "./MyComponent"
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <h1>abcdefgh</h1>
+            {/* <h1>abcdefgh</h1> */}
+        <MyComponent/>
+        
         </>
     )
 }
